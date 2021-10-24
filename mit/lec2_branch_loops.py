e07ca48f53315db04b196c9e845b7cf8d2ccc1fb
Sergio Juarez <sergiojc90@gmail.com>
2021-10-24 15:57:36 -0500

    Spaces and input2

--- mit/lec2_branch_loops.py
+++ mit/lec2_branch_loops.py
@@ -12,3 +12,17 @@ print(greeting)
 
 silly = hi + (" " + name)*3
 print(silly)
+
+###################
+
+x = 1
+print(x)
+
+x_str = str(x)
+print("my fav number is", x,".","x =",x)
+print("my fav number is", x_str + ".","x = "+x_str)
+
+##################
+
+text = input("Type anything... ")
+print(5*text)
