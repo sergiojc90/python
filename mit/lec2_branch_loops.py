237697a2eca2902eb8167597efce55a991a81d1b
Sergio Juarez <sergiojc90@gmail.com>
2021-10-24 14:15:01 -0500

    MIT python course

---
+++ mit/lec2_branch_loops.py
@@ -0,0 +1,14 @@
+###################
+# Example: Strings#
+###################
+
+hi = "Hello there"
+name = "ana"
+greet = hi + name
+print(greet)
+
+greeting = hi + " " + name
+print(greeting)
+
+silly = hi + (" " + name)*3
+print(silly)
