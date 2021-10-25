5fc97ad8d47aa50723f9a52666ffc4ecf57a80bd
Sergio Juarez <sergiojc90@gmail.com>
2021-10-24 22:23:26 -0500

    range (start,stop)

--- mit/lec2_branch_loops.py
+++ mit/lec2_branch_loops.py
@@ -38,16 +38,42 @@
 # #############
 # #Control Flow
 
-x = float(input("Enter a number for x: "))
-y = float(input("Enter a number for y: "))
+# If condition
 
-if x == y:
-    print("x and y are equal")
-    if y != 0:
-        print("therefore, x/y is", x/y)
-elif x < y:
-    print("x is smaller")
-else:
-    print("y is smaller")
+# x = float(input("Enter a number for x: "))
+# y = float(input("Enter a number for y: "))
 
-print("thanks!")
+# if x == y:
+#     print("x and y are equal")
+#     if y != 0:
+#         print("therefore, x/y is", x/y)
+# elif x < y:
+#     print("x is smaller")
+# else:
+#     print("y is smaller")
+
+# print("thanks!")
+
+# While loop
+
+# n = 0
+
+# while n <= 5:
+#     print(n)
+
+# for n in range(5):
+#     print(n)
+
+# Range (start,stop,step)
+
+mysum = 0
+
+for i in range(7, 10):
+    mysum += i
+
+print(mysum)
+
+mysum = 0
+for i in range(5, 11, 2):
+    mysum += i
+print(mysum)
