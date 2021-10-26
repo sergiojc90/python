8fa5979d62a20852871322e1df186524761ffd02
Sergio Juarez <sergiojc90@gmail.com>
2021-10-25 23:41:30 -0500

    Using for loop to iterate through a string

--- mit/lec3_strings.py
+++ mit/lec3_strings.py
@@ -14,5 +14,19 @@
 
 ## Strings are immutable
 
-s = "hello"
-s[0] = "y"
+# s = "hello"
+# s[0] = "y"
+
+## for Loops Recap
+## These two code snippets do the same thing
+
+s = "abcdefgh"
+
+for index in range(len(s)):
+    if s[index] == "i" or s[index] == "u":
+        print("There is an i or u")
+
+## This snippet is more "pythonic"
+for char in s:
+    if char == "i" or char == "u":
+        print("There is an i or u")
