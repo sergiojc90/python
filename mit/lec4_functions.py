1be6df7935ca2b16bcd63cf2db27153a5dee529b
Sergio Juarez <sergiojc90@gmail.com>
2021-10-29 23:25:44 -0500

    functions and for loop

--- mit/lec4_functions.py
+++ mit/lec4_functions.py
@@ -62,14 +62,33 @@
 # is_even_with_return(3)
 # print(is_even_with_return(3))
 
-def is_even_without_return(i):
+# def is_even_without_return(i):
+#     """
+#     Input: i, a positive int
+#     Does not return anything
+#     """
+#     print("without return")
+#     remainder = i % 2
+#     # return none
+
+# is_even_without_return(3)
+# print(is_even_without_return(3))
+
+# Simple is_even function definition
+def is_even(i):
     """
     Input: i, a positive int
-    Does not return anything
+    Returns True if i is even, otherwise False
     """
-    print("without return")
-    remainder = i % 2
-    # return none
 
-is_even_without_return(3)
-print(is_even_without_return(3))
+    remainder = i % 2
+    return remainder == 0
+
+# Use the even function later on in the code
+print("All numbers between 0 and 20: even or not")
+
+for i in range(20):
+    if is_even(i):
+        print(i, "even")
+    else:
+        print(i, "odd")
