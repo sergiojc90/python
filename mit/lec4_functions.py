77cff162626c4fceb29415649b84468fd2e996a0
Sergio Juarez <sergiojc90@gmail.com>
2021-10-29 22:34:09 -0500

    Functions definition and characteristics

--- mit/lec4_functions.py
+++ mit/lec4_functions.py
@@ -10,4 +10,16 @@
 ## In this lecture, achive decomposition with FUNCTIONS
 ## Decomposition can also be achived with CLASES
 #######################
-
+## Supress details with abstraction
+## Inputs, type of inputs, what it does, output
+#######################
+## Functions (Hat1 - Create the function, Hat2 - User of the function)
+## Reusable pieces/chunks of code
+## Functions are not fun in a program until they are "called" of "invoked" in a program
+## function characteristics:
+## has a name
+## has parameteres (0 or more)
+## has a docstring (optional but recomended)
+## has a body
+## RETURNS something
+##
