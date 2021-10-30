518d5170962e7c332bfb4280d7e3c17ebfe3ca44
Sergio Juarez <sergiojc90@gmail.com>
2021-10-29 22:24:08 -0500

    Description

---
+++ mit/lec4_functions.py
@@ -0,0 +1,13 @@
+#######################
+## Create Structure with Decomposition
+## Divide Code into modules
+## - Sefl-contained
+## - Break up the code
+## - Reusable
+## - Organized
+## - Keep code coherent
+##
+## In this lecture, achive decomposition with FUNCTIONS
+## Decomposition can also be achived with CLASES
+#######################
+
