3f7f505875c40c9abb2901d8b4a00e7d604f92cd
Sergio Juarez <sergiojc90@gmail.com>
2021-10-29 23:14:38 -0500

    is_even_with_return function

--- mit/lec4_functions.py
+++ mit/lec4_functions.py
@@ -49,3 +49,15 @@
 ##
 ## x = 3
 ## z = f(x)
+
+def is_even_with_return(i):
+    """
+    Input: i, a positive int
+    Return True if i is even, otherwise False
+    """
+    print("with return")
+    remainder = i % 2
+    return remainder == 0
+
+is_even_with_return(3)
+print(is_even_with_return(3))
