faec4493f09ced0609f1e24b1f51220288c737ff
Sergio Juarez <sergiojc90@gmail.com>
2021-10-31 12:45:02 -0600

    Lecture 5, tuples, listing, aliasing, mutability, cloning

---
+++ mit/lec5_tuples.py
@@ -0,0 +1,16 @@
+################################
+## Lecture 5
+## Tuples, Listing, Aliasing, Mutability, Cloning
+##
+## Compound Data Types
+## - Tuples
+## - Lists
+## Idea of aliasing
+## Idea of mutability
+## Idea of cloning
+##
+## TUPLES
+## An ordered sequence of elements, can mix element types cannot change element values, immutable represented with parenthesis
+te = () # Empty tuple
+t = (2,"mit",3)
+t[0]
