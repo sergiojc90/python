bec26dda11b99ea96cd65318378bd758cc6c590a
Sergio Juarez <sergiojc90@gmail.com>
2021-10-31 13:10:44 -0600

    Using tuples to return colection of date from a function

--- mit/lec5_tuples.py
+++ mit/lec5_tuples.py
@@ -11,9 +11,18 @@
 ##
 ## TUPLES
 ## An ordered sequence of elements, can mix element types cannot change element values, immutable represented with parenthesis
-te = () # Empty tuple
-t = (2,"mit",3)
-print(t+(5,6))
-print(te)
-print(len(t))
-t[1] = 4
+# te = () # Empty tuple
+# t = (2,"mit",3)
+# print(t+(5,6))
+# print(te)
+# print(len(t))
+# t[1] = 4
+## Tuples can be used to return more than one value from a function
+def quotient_and_remainder(x,y):
+    q = x // y
+    r = x % y
+    return(q,r)
+
+(quot,rem) = quotient_and_remainder(4,5)
+print(quot)
+print(rem)
