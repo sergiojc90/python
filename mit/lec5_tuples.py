5c285f41161a7e5bd90a67972f22721d8399f058
Sergio Juarez <sergiojc90@gmail.com>
2021-10-31 14:26:05 -0600

    Iterating within a tuple

--- mit/lec5_tuples.py
+++ mit/lec5_tuples.py
@@ -18,11 +18,32 @@
 # print(len(t))
 # t[1] = 4
 ## Tuples can be used to return more than one value from a function
-def quotient_and_remainder(x,y):
-    q = x // y
-    r = x % y
-    return(q,r)
+# def quotient_and_remainder(x,y):
+#     q = x // y
+#     r = x % y
+#     return(q,r)
 
-(quot,rem) = quotient_and_remainder(4,5)
-print(quot)
-print(rem)
+# (quot,rem) = quotient_and_remainder(4,5)
+# print(quot)
+# print(rem)
+#
+## We can iterate within a tuple
+
+def get_data(aTuple):
+    nums = ()
+    words = ()
+
+    for t in aTuple:
+        nums = nums + (t[0],)
+        if t[1] not in words:
+            words = words + (t[1],)
+
+    min_n = min(nums)
+    max_n = max(nums)
+    unique_words = len(words)
+
+    return(min_n, max_n, unique_words, words)
+
+myTuple = ((1,"Pizza"), (2, "Burger"),(3,"Tacos"),(4,"Hotdog"),(1,"Burger"))
+
+print(get_data(myTuple))
