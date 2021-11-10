9633f4af48ff01fdac3ff7e8b4dc18c5e1c5742d
Sergio Juarez <sergiojc90@gmail.com>
2021-11-09 21:20:16 -0600

    Classes of tests

--- lec7_testing_debuggin_exceptions_assertions.py
+++ lec7_testing_debuggin_exceptions_assertions.py
@@ -13,3 +13,17 @@
 ### - Study events leading up to an error
 ### - Why is not working?
 ### - How can I fix my program?
+###
+### CLASSES OF TESTS
+### 
+### UNIT TESTING
+### - Validate each piece of program
+### - Testing each function separately
+### 
+### REGRESSION TESTING
+### - Add test for bugs as you find them
+### - Catch reintroduced errors that were previously fixed
+###
+### INTEGRATION TESTING
+### - Does overall program work?
+### - Tend to rush to do this
