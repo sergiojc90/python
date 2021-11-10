429e2fb6d44626f12805346913efd6a7de278a87
Sergio Juarez <sergiojc90@gmail.com>
2021-11-09 21:03:54 -0600

    Lecture 7, Testing, debuggin, exceptions, assertions

---
+++ lec7_testing_debuggin_exceptions_assertions.py
@@ -0,0 +1,15 @@
+###########################
+### DEFENSIVE PROGRAMMING
+### - Write specifications for functions
+### - Modulirize programs
+### - Check conditions on inputs/outputs (assertions)
+###
+### TESTING/VALIDATION
+### - Compare input/output pairs to specification
+### - It's not working
+### - How can I break my program?
+###
+### DEBUGGING
+### - Study events leading up to an error
+### - Why is not working?
+### - How can I fix my program?
