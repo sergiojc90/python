811f8417e70016670b9e424c338057d67ad227ad
Sergio Juarez <sergiojc90@gmail.com>
2021-11-10 22:32:54 -0600

    Classes

---
+++ mit/lec8_OOP.py
@@ -0,0 +1,36 @@
+### OBJECT ORIENTED PROGRAMMING
+### 
+### Python objects supports many different kinds of data
+###
+### each is an object, and every object has:
+### - a type
+### - an internal data representation
+### - a set of procedures for interaction with the object
+### 
+### - an object is an instance of a type
+### 1234 is an intance of an int
+### "hello" is an instance of a string
+###
+### EVERYTHING IN PYTHON IS AN OBJECT
+###
+### You can create, manipulate and destroy objects (using del or just forgetting about them)
+###
+### Objects are a DATA ABSTRACTION
+### 1) An internal representation through data attributes
+### 2) An interface for interactiong with object
+### - through methods
+### - defines behaviors but hides implementation
+###
+### CREATING AND USING YOUR OWN TYPES WITH CLASSES
+### 
+### Make distinction between CREATING A CLASS and USING INSTANCE OF THE CLASS
+###
+### Creating the class involves
+### - Defining the class name
+### - Defining class atributes
+### - for example, someone wrote code to implement a list class
+###
+### Using the class involves
+### - Creating new instances of objects
+### - Doing operations on the instances
+### - for example, L=[1,2] and len(L)
