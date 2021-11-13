f2ff4d6fd97763af6b344a2feeefa1ad3aed25a3
Sergio Juarez <sergiojc90@gmail.com>
2021-11-12 22:52:14 -0600

    Class definition and instance of a class

---
+++ mit/lec9_Classes_and_Inheritance.py
@@ -0,0 +1,34 @@
+### IMPLEMENTING THE CLASS VS USING THE CLASS
+###
+### Write code from two different perspectives
+###
+### Implementing a new object type with a class
+### - Define the class
+### - Define data attributes (what is the object)
+### - define methods (how to use the object)
+###
+### Using the new object type in code
+### - create instances of the type
+### - do operations with them
+
+### CLASS DEFINITION OF AN OBJECT TYPE
+### Class name is the type
+### Class coordinate(object)
+
+### Class is defined generically
+### use self to refer to some instance while defining the class
+### (self.x - self.y)**2
+###
+### self is a parameter to methods in class definition
+###
+### class defines data and methods common across all instances
+###
+### INSTANCE OF A CLASS
+### instance is one specific object
+### coord = Coordinate(1,2)
+
+### data attribute values vary between instances
+### c1 = Coordinate(1,2)
+### c2 = Coordinate(3,4)
+
+### c1 and c2 have different data attribute values c1.x and c2.x because they are different objects
