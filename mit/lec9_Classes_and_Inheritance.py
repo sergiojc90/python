64ca4c43578c048d66989468b408e92392a024b7
Sergio Juarez <sergiojc90@gmail.com>
2021-11-12 22:59:26 -0600

    Getter and setter methods

--- mit/lec9_Classes_and_Inheritance.py
+++ mit/lec9_Classes_and_Inheritance.py
@@ -37,3 +37,22 @@
 ### Getters return the values of any of the data attributes
 ### Setters set the data attributes to whatever is set in
 
+class Animal(object):
+    def __init__(self,age):
+        self.age = age
+        self.name = None
+
+    def get_age(self):
+        return self.age
+
+    def get_name(self):
+        return self.name
+    
+    def set_age(self,newage):
+        self.age = newage
+    
+    def set_name(self, newname = ""):
+        self.name = newname
+    
+    def __str__(self):
+        return "Animal:" + str(self.name) + ":" + str(self.age)
